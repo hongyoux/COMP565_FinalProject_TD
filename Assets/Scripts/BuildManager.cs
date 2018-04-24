6fd00726e84a7e579170008345a230b3aa8cc58f
Hongyou Xiong <hongyoux@gmail.com>
2018-04-24 03:41:56 -0700

    Can now place turrets
    Can place turrets by clicking on squares that hover green. No resource system yet.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private GameObject mSelectedBuilding;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager is in the scene! BAD");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject mStandardTurret;
+
+    private void Start()
+    {
+        mSelectedBuilding = mStandardTurret;
+    }
+
+    public GameObject GetBuildingToBuild()
+    {
+        return mSelectedBuilding;
+    }
+}
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class Node : MonoBehaviour {
 
     public Color mHoverColor;
+    public Vector3 mPosOffset;
+
+    private GameObject mBuilding;
+
+    private Renderer mRend;
+    private Color mOriginalColor;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        mRend = GetComponent<Renderer>();
+        mOriginalColor = mRend.material.color;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    private void OnMouseDown()
+    {
+        if (mBuilding != null)
+        {
+            Debug.Log("Something is already here!");
+            return;
+        }
+
+        GameObject newTurret = BuildManager.instance.GetBuildingToBuild();
+        mBuilding = Instantiate(newTurret, transform.position + mPosOffset, transform.rotation);
+    }
+
     private void OnMouseEnter()
     {
-        GetComponent<Renderer>().material.color = mHoverColor;
+        if (mBuilding == null)
+        {
+            mRend.material.color = mHoverColor;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        mRend.material.color = mOriginalColor;
     }
 }
